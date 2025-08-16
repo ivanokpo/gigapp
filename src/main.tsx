ed33b47b8e21d4ed5e2618e35f500e2d03aa991b
Admin <admin@Admins-MacBook-Pro.local>
2025-08-16 18:28:32 +0100

    fix: tidy up

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {App} from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { EventsPage } from './pages/events/EventsPage.tsx'
 import {Layout} from './components/Layout.tsx'
